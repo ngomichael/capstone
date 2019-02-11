69ac467b087184702cb71ea2ef0f75d3c3a77e54
ngomichael <michael_ngo@live.com>
2019-02-11 13:39:50 -0800

    added CSS modules and updated webpack dev server to latest version

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import { hot } from 'react-hot-loader';
-import './App.css';
+// import './App.css';
+import styles from './App.module.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.app}>
         <h1> Hello, World!</h1>
       </div>
     );
