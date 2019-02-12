722d6a7e46fafd8b1995844d01dc764e6ba48eef
ngomichael <michael_ngo@live.com>
2019-02-11 18:37:47 -0800

    created basic counter using redux

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import { hot } from 'react-hot-loader'
 import styles from './App.module.css'
+import Counter from './counter/Counter'
 
 class App extends Component {
   render() {
     return (
       <div className={styles.app}>
-        <h1> Hello, World!</h1>
+        <h1> Hello, World! Welcome!</h1>
+        <Counter />
       </div>
     )
   }
