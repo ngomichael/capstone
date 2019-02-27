47d64e015c4f9e5c735b9afa06f906f83d9bbf62
ngomichael <michael_ngo@live.com>
2019-02-26 17:25:50 -0800

    added all four provider cards and is responsive

--- src/questionnaire/Card.js
+++ src/questionnaire/Card.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import styles from './Card.module.css'
+import PropTypes from 'prop-types'
 
 const Card = props => {
   return (
     <div className={styles.container}>
       <div className={styles.icon} />
       <div className={styles.textContainer}>
-        <p className={styles.therapistType}>Therapist</p>
-        <p className={styles.therapistDescription}>The ones that talk</p>
+        <p className={styles.providerType}>{props.providerType}</p>
+        <p className={styles.providerDescription}>
+          {props.providerDescription}
+        </p>
       </div>
     </div>
   )
+}
+
+Card.propTypes = {
+  providerType: PropTypes.string,
+  providerDescription: PropTypes.string,
 }
 
 export default Card
--- src/questionnaire/QuestionnaireModal.js
+++ src/questionnaire/QuestionnaireModal.js
@@ -2,6 +2,25 @@ import React from 'react'
 import styles from './QuestionnaireModal.module.css'
 import Card from './Card'
 
+const providerTypes = [
+  {
+    providerType: 'Therapist',
+    providerDescription: 'The ones that talk to people',
+  },
+  {
+    providerType: 'Psychiatrist',
+    providerDescription: 'The ones that medicate',
+  },
+  {
+    providerType: 'Psychological Testing',
+    providerDescription: 'The ones that check your kids',
+  },
+  {
+    providerType: 'Not sure/None of these',
+    providerDescription: "The ones that don't know",
+  },
+]
+
 const QuestionnaireModal = props => {
   return (
     <div className={styles.container}>
@@ -15,7 +34,14 @@ const QuestionnaireModal = props => {
             Not sure? Skip to the next question >
           </p>
         </div>
-        <Card />
+        <div className={styles.cardContainer}>
+          {providerTypes.map(provider => (
+            <Card
+              providerType={provider.providerType}
+              providerDescription={provider.providerDescription}
+            />
+          ))}
+        </div>
       </div>
     </div>
   )
