df063bbe9d25f7bef675e64aeb51213be406a5f0
Daniel Colina <colind4@uw.edu>
2019-02-28 00:32:37 -0800

    add confirmation page

--- src/confirmation/Confirmation.js
+++ src/confirmation/Confirmation.js
@@ -1,14 +1,18 @@
 import React, { useState } from 'react'
 import { Link} from '@reach/router'
 import styles from './Confirmation.module.css'
-import firebase from '../Firebase/firebase'
 
 const Confirmation = props => {
     
 return (
-    <div>
-        <p> Thank you for creating an account! </p>
+    <div className={styles.container}>
+        <div className={styles.text_content}>
+        <p className={styles.header}> Thank you for creating an account! </p>
         <p>  A confirmation email has been sent to your email at {props.email} </p>
+         <p> To get the best therapist pair for your care, we recommend taking a questionnaire prepared for you.</p>
+        </div>
+        <button className={styles.button}> Start questionnaire </button>
+        <Link to='/'> Skip to a list of providers near me </Link>
     </div>
 
 )
