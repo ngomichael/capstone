48ea5cfbe77fba1bdd2a01d29e93758d079eb483
Daniel Colina <colind4@uw.edu>
2019-03-05 17:52:02 -0800

    add changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, {useEffect, useState} from 'react'
-import { hot } from 'react-hot-loader'
 import { Router } from '@reach/router'
 import styles from './App.module.css'
 import SignUp from './signUp/SignUp'
