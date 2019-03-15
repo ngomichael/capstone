3a43ef8b3360da054b812b2351b79ab821a30a8a
ngomichael <michael_ngo@live.com>
2019-03-14 20:04:18 -0700

    added back button and footer to provider info page

--- src/providerInfo/ProviderInfo.js
+++ src/providerInfo/ProviderInfo.js
@@ -13,8 +13,26 @@ const ProviderInfo = () => {
 
   return (
     <div className={styles.container}>
-      {isOpen && <ContactModal handleClose={handleClose} />}
+      {/* {isOpen && <ContactModal handleClose={handleClose} />} */}
       <QuesionnaireHeader step={2} />
+      {/* Needs to be a link here to go back */}
+      <div className={styles.backButton}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          stroke="currentColor"
+          stroke-width="2"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          class="feather feather-chevron-left"
+        >
+          <polyline points="15 18 9 12 15 6" />
+        </svg>
+        <p>Back</p>
+      </div>
       <div className={styles.providerCardContainer}>
         <div style={{ display: 'flex', alignItems: 'center' }}>
           <div className={styles.providerPhoto} />
@@ -111,6 +129,36 @@ const ProviderInfo = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className={styles.footer}>
+        <div className={styles.leftSideActions}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="#1AAE9F"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            style={{ marginRight: '20px', marginLeft: '20px' }}
+            className={styles.favorite}
+          >
+            <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+          </svg>
+          <a
+            href="https://www.google.com"
+            rel="noreferrer noopener"
+            target="_blank"
+            style={{ color: '#149184' }}
+          >
+            Get Directions
+          </a>
+        </div>
+        <button className={styles.interestedButton}>
+          I'm interested in reaching out
+        </button>
       </div>
     </div>
   )
