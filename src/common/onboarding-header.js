386c1a9bef1603d9dcb8c46d0740f3a4f5037ec1
ngomichael <michael_ngo@live.com>
2019-04-25 14:57:33 -0700

    updated pearcare icon for all headers

--- src/common/onboarding-header.js
+++ src/common/onboarding-header.js
@@ -2,13 +2,13 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import styles from './onboarding-header.module.css'
 import { OnboardingTracker } from './onboarding-tracker'
-import PearCareLogo from '../images/logo.png'
+import PearCareIcon from '../icons/pearcare-icon.png'
 
 export const OnboardingHeader = ({ step }) => {
   return (
     <header className={styles.container}>
       <div className={styles.logoAndNameContainer}>
-        <img src={PearCareLogo} className={styles.logo} />
+        <img src={PearCareIcon} className={styles.logo} />
         <p className={styles.productName}>PearCare</p>
       </div>
       <div className={styles.onboardingTracker}>
